b977473611e3e39baccdcf403dae4ad80423b2da
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-01-09 12:55:39 -0500

    Update dpol.m
    replaced diff operations with built-in functions

--- auxiliaryFunctions/dpol.m
+++ auxiliaryFunctions/dpol.m
@@ -7,7 +7,6 @@ function [dth,dr] = dpol(thvec,rvec)
 %
 % See also: DCART
 
-dth = atan2(rvec(2:end).*sin(thvec(2:end))-rvec(1:end-1).*sin(thvec(1:end-1)),...
-    rvec(2:end).*cos(thvec(2:end))-rvec(1:end-1).*cos(thvec(1:end-1))); 
-dr  = sqrt(rvec(1:end-1).^2 + rvec(2:end).^2 - 2.*rvec(1:end-1).*rvec(2:end).*cos(thvec(2:end)-thvec(1:end-1)));
+dth = atan2(diff(rvec.*sin(thvec)),diff(rvec.*cos(thvec)));
+dr  = sqrt(rvec(1:end-1).^2 + rvec(2:end).^2 - 2.*rvec(1:end-1).*rvec(2:end).*cos(diff(thvec)));
 end
