75e35e44f802939c8fad03a4cc342ca354a3e7f7
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-01-11 12:05:55 -0500

    Update generateTrajectorySegment.m
    replaced numerical integration with closed-form solution for cartesian coordinates of trajectory and curvilinear distance; updated function description accordingly

--- generateTrajectorySegment.m
+++ generateTrajectorySegment.m
@@ -1,18 +1,17 @@
-function [xtraj,ytraj,vtraj,htraj] = generateTrajectorySegment(t,dth,dr,phi,T)
+function [xtraj,ytraj,vtraj,htraj,d] = generateTrajectorySegment(t,dth,dr,phi,T)
 % GENERATETRAJECTORYSEGMENT Generates a trajectory that, once appropriately
 % offset, links a pair of anchor points.
-%   [xtraj,ytraj] = GENERATETRAJECTORYSEGMENT(t,dth,dr,phi,T) takes as input  
-%   an ordered pair of anchor points that are defined in polar coordinates 
-%   based on their angular separation 'dth' and their separation 'dr', 
-%   an initial heading angle 'phi', and a total duration 'T'. It returns 
-%   the cartesian coordinates of a curvilinear trajectory ('xtraj' and 
-%   'ytraj'), together with the instantaneous heading ('htraj') and speed 
-%   ('vtraj'). All outputs are defined over a time vector 't'. This 
-%   trajectory is defined in relative coordinates (based on the relative 
+%   [xtraj,ytraj,vtraj,htraj,d] = GENERATETRAJECTORYSEGMENT(t,dth,dr,phi,T) 
+%   takes as input an ordered pair of anchor points that are defined in 
+%   polar coordinates based on their angular separation 'dth' and their 
+%   separation 'dr', an initial heading angle 'phi', and a total duration 
+%   'T'. It returns the cartesian coordinates of a curvilinear trajectory 
+%   ('xtraj' and 'ytraj'), together with the instantaneous heading ('htraj') 
+%   and speed ('vtraj'). All outputs are defined over a time vector 't'.
+%   This trajectory is defined in relative coordinates (based on the relative 
 %   separation between anchor points); to position it in absolute 
 %   coordinates, it must be offset by the [x,y] location of the first 
-%   anchor point, and scaled based on the temporal discretization of the
-%   trajectory (stored in planner.tScale).
+%   anchor point.
 %
 %   See also: PLANTRAJECTORY, RECOVERCONTROLPARAMS 
 
@@ -23,13 +22,24 @@ function [xtraj,ytraj,vtraj,htraj] = generateTrajectorySegment(t,dth,dr,phi,T)
 %   phi = initial heading offset, defined relative to dth-pi/2
 %   T   = total time to travel between two anchors
 
-% generate speed and heading over time
-A = (dr./T).*(pi-2*phi).*(pi+2*phi).*(3*pi-2*phi)./(4.*pi.^2.*cos(phi));
-vtraj = A.*.5*(1-cos(t.*2.*pi./T));
-htraj = ((pi-2*phi).*t./T+phi+dth-pi/2);
+% define auxiliary parameters
+m     =  (  pi-2*phi)./T;
+alpha =  (3*pi-2*phi)./T;
+beta  = -(  pi+2*phi)./T;
+omega =  (2*pi      )./T;
+delta = phi+dth-pi/2;
 
-% integrate speed and heading to get position
-xtraj = cumsum(vtraj.*cos(htraj));
-ytraj = cumsum(vtraj.*sin(htraj));
+% generate speed and heading over time
+A = (-dr.*m.*alpha.*beta)./(2.*omega.^2.*cos(phi));
+vtraj = A.*(1-cos(omega.*t));
+htraj = m*t+delta;
+
+% generate cartesian trajectory (closed-form solution to the integrals
+% xtraj = cumsum(vtraj.*cos(htraj)) and ytraj = cumsum(vtraj.*sin(htraj))
+xtraj = -dr.*(2.*omega.^2.*sin(delta)+alpha.*(2.*beta.*sin(delta+m.*t)-m.*sin(delta+beta.*t))-m.*beta.*sin(delta+alpha.*t))./(4.*omega.^2.*cos(phi));
+ytraj =  dr.*(2.*omega.^2.*cos(delta)+alpha.*(2.*beta.*cos(delta+m.*t)-m.*cos(delta+beta.*t))-m.*beta.*cos(delta+alpha.*t))./(4.*omega.^2.*cos(phi));
+
+% compute curvilinear distance along trajectory
+d = abs(A.*T);
 
 end
