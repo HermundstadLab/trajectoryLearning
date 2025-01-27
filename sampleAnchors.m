d45e1d48563f7dd2980db424e018f3b8d1573dad
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-01-27 15:39:36 -0500

    changed input structure to sampleAnchors.m
    allow user to specify the fraction of probability mass covered by selected anchor points

--- sampleAnchors.m
+++ sampleAnchors.m
@@ -1,46 +1,52 @@
-function anchors = sampleAnchors(prior,belief,sampler,planner)
+function anchors = sampleAnchors(map,belief,sampler,planner,frac)
 % SAMPLEANCHORS Sample anchor points from a probability distribution.
 %
-%   anchors = SAMPLEANCHORS(prior,belief,sampler,planner) finds peaks in a 
-%   prior probability distribution that are of a min height and separation 
-%   (as specified in the 'belief' structure'). It then selects a fraction 
-%   of these peaks as sampled anchor points; if there are no peaks in the 
-%   distribution, this function randomly samples the maximum number of 
-%   allowed peaks (as specified in the 'sampler' structure). The polar 
-%   coordinates of these anchors are stored in the output structure 
+%   anchors = SAMPLEANCHORS(map,belief,sampler,planner,frac) finds peaks 
+%   in a 2D landscape ('map') that are of a min height and separation 
+%   (as specified in the 'belief' structure'). It then selects the minimum
+%   subset of these peaks that cover a fraction 'frac' of their summed
+%   values; this subset will be returned as sampled anchor points. If there
+%   are no peaks in the map, this function randomly samples the maximum 
+%   number of allowed peaks (as specified in the 'sampler' structure). The 
+%   polar coordinates of these anchors are returned in the output structure 
 %   'anchors'.  
 %
 %   See also: PEAKS2
 
-% determine peaks in the prior; these will be used to sample anchors              
-priorTmp = prior;                               % define temporary variable for sampling
-priorTmp(isnan(priorTmp)) = 0;                  % remove nans for sampling (allow borders to be peaks)
-[pks,locs_r,locs_th] = peaks2(priorTmp,...      % determine peaks
+% function defaults to sampling anchors that cover 50% of probability mass:
+if nargin<5
+    frac = 0.5;
+end
+
+% determine peaks in the map; these will be used to sample anchors              
+mapTmp = map;                               % define temporary variable for sampling
+mapTmp(isnan(mapTmp)) = 0;                  % remove nans for sampling (allow borders to be peaks)
+[pks,locs_r,locs_th] = peaks2(mapTmp,...      % determine peaks
     'MinPeakDistance',sampler.minPeakDist,...
     'MinPeakHeight',sampler.minPeakHeight);     
 [pkssort,indsort] = sort(pks,'descend');
 
 if numel(pks)<1
-    % if there are no peaks in the prior, randomly sample the maximum number of anchors
+    % if there are no peaks in the map, randomly sample the maximum number of anchors
     nk = sampler.nAnchorsMax;
 
     % remove nans before sampling
-    lininds = find(~isnan(prior));
+    lininds = find(~isnan(map));
 
     % randomly sample nk anchors
     indperm = randperm(numel(lininds),nk);
 
     % extract polar coordinates of anchors
-    [indr,indth] = ind2sub(size(prior),lininds(indperm));
+    [indr,indth] = ind2sub(size(map),lininds(indperm));
     anchors.thCoords = belief.thAxes(indth);                                           
     anchors.rCoords  = belief.rAxes( indr ); 
     anchors.thTol    = planner.thTol_shift*ones(1,numel(indth));
     anchors.rTol     = planner.rTol_shift*ones( 1,numel(indr ));
 else
     
-    % if there are peaks in the prior, choose the smallest number of peaks
+    % if there are peaks in the map, choose the smallest number of peaks
     % that cover half of the total peak probability mass
-    nk = find(cumsum(pkssort./sum(pkssort))>.5,1,'first');
+    nk = find(cumsum(pkssort./sum(pkssort))>=frac,1,'first');
 
     % select top peaks to be anchor points
     indsel = indsort(1:nk);                                           
