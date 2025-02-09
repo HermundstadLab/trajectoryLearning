d0c91cec14b63a558a605a40d15ff986a0ec6aed
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-02-09 11:48:35 -0500

    updated notation
    added boundary flag output, stored time increment in planner structure

--- planTrajectory.m
+++ planTrajectory.m
@@ -1,13 +1,17 @@
-function trajectory = planTrajectory(anchors,phi,planner)
+function trajectory = planTrajectory(anchors,delta,planner,boundaryFlag)
 % PLANTRAJECTORY Plan a trajectory through a set of anchor points.
-%   trajectory = PLANTRAJECTORY(anchors,phi,planner) uses a set of ordered 
+%   trajectory = PLANTRAJECTORY(anchors,delta,planner) uses a set of ordered 
 %   anchor points (defined in polar coordinates and stored in the structure 
-%   'anchors') and an initial heading angle phi to plan a curvilinear 
+%   'anchors') and an initial heading angle delta to plan a curvilinear 
 %   trajectory through them.  The input 'planner' structure contains 
 %   parameters that specify the temporal discretization and overall scaling 
 %   of the trajectory.
 %
 %   See also: ORDERANCHORS, GENERATETRAJECTORY, OPTIMIZETRAJECTORY, EXECUTETRAJECTORY 
+
+if nargin<4
+    boundaryFlag = false;
+end
 
 % initialize trajectory
 [x0,y0] = pol2cart(anchors.thCoords(1),anchors.rCoords(1));
@@ -15,7 +19,7 @@ dth0    = 0;
 
 % store anchors and initial heading
 trajectory.anchors         = anchors;
-trajectory.phi             = phi;
+trajectory.delta           = delta;
 
 % initialize trajectory properties
 trajectory.prevAnchor = [];
@@ -23,10 +27,9 @@ trajectory.xCoords    = [];
 trajectory.yCoords    = [];
 trajectory.velocity   = [];
 trajectory.heading    = [];
-trajectory.timepts    = [];
 trajectory.distance   = 0;
 
-dt = (1/planner.nInterp);
+trajectory.boundaryFlag = boundaryFlag;
 
 for i=2:anchors.N
     
@@ -36,25 +39,26 @@ for i=2:anchors.N
     % scale execution time based on separation between anchors, requiring
     % at least two timepoints per segment
     T  = dr./planner.rScale; 
-    tt = 0:dt:T;
+    tt = 0:planner.dt:T;
 
     % if generating last segment of trajectory, append final timepoint 
     if i==anchors.N && tt(end)<T
        tt = [tt,T]; 
     end
 
-    % ensure initial offset is the same as final offset from last segment
-    if i>2
-        phi = wrapToPi(pi-(dth-dth0)-phi0);
+    % for any trajectory besides a boundary trajectory, ensure the initial 
+    % offset is the same as final offset from last segment
+    if i>2 && ~boundaryFlag
+        delta = wrapToPi(pi-(dth-dth0)-delta0);
     end
 
     % generate trajectory linking anchors; choose multiple of initial  
     % heading offset that minimizes curvilinear distance
-    phiSet = [phi-2*pi,phi,phi+2*pi];
-    dist   = nan(size(phiSet));
-    [xtmp,ytmp,vtmp,htmp] = deal(nan(size(phiSet,2),numel(tt)));
+    deltaSet = [delta-2*pi,delta,delta+2*pi];
+    dist   = nan(size(deltaSet));
+    [xtmp,ytmp,vtmp,htmp] = deal(nan(size(deltaSet,2),numel(tt)));
     for j=1:3
-        [xtraj,ytraj,vtmp(j,:),htmp(j,:),dist(j)] = generateTrajectorySegment(tt,dth,dr,phiSet(j),T);
+        [xtraj,ytraj,vtmp(j,:),htmp(j,:),dist(j)] = generateTrajectorySegment(tt,dth,dr,deltaSet(j),T);
         xtmp(j,:) = x0+xtraj;
         ytmp(j,:) = y0+ytraj;
     end
@@ -69,10 +73,10 @@ for i=2:anchors.N
     trajectory.distance   = trajectory.distance + dist(isel); 
 
     % update initial condition
-    x0   = xtmp(isel,end);
-    y0   = ytmp(isel,end);
-    dth0 = dth;
-    phi0 = phi;
+    x0     = xtmp(isel,end);
+    y0     = ytmp(isel,end);
+    dth0   = dth;
+    delta0 = delta;
     
 end
 
