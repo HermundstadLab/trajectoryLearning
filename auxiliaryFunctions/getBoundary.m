a68c91ba7c3c478a4d10eb3b94094315c82d9579
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-02-10 10:02:49 -0500

    added output that stores corner positions
    this is used for generating boundary trajectories

--- auxiliaryFunctions/getBoundary.m
+++ auxiliaryFunctions/getBoundary.m
@@ -1,12 +1,13 @@
-function [xBoundary,yBoundary,heading,indSegments,binSegments] = getBoundary(xBounds,yBounds,np)
+function [xBoundary,yBoundary,heading,indSegments,binSegments,corners] = getBoundary(xBounds,yBounds,np)
 % GETBOUNDARY Returns densely-sampled coordinates of rectangular area.
-%   [xBounds,yBounds,heading,indSegments,binSegments] = GETBOUNDS(xBounds,yBounds,np) 
+%   [xBounds,yBounds,heading,indSegments,binSegments,corners] = GETBOUNDS(xBounds,yBounds,np) 
 %   uses np points per unit distance to interpolate each of the boundaries 
 %   specified by xBounds = [xmin,xmax] and yBounds = [ymin,ymax], and 
 %   returns the x- and y-coordinates of the boundary, together with the 
 %   heading of a trajectory that traverses the same boundary. Individual
 %   boundaries are specified by the indices in 'indSegments'; boundary IDs
-%   are specified by 'binSegments'.
+%   are specified by 'binSegments', and corner positions by the nonzero
+%   entries in 'corners'.
 
 xBoundary = [linspace(xBounds(1),xBounds(2),floor(np*diff(xBounds))),...
     linspace(xBounds(2),xBounds(2),floor(np*diff(yBounds))),...
@@ -32,6 +33,8 @@ for i=1:4
     indSegments{i} = bounds(i)+1:bounds(i+1);
     binSegments = [binSegments,bounds(i)+1];
 end
+corners = zeros(size(xBoundary));
+corners(binSegments) = 1;
 binSegments = [binSegments,bounds(end)+1];
 
 end
