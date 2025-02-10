85fece955863e04247c8024ff48f5735a05ba798
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-02-10 14:18:32 -0500

    added field for averaging window (for plotting)

--- loadPlotParams.m
+++ loadPlotParams.m
@@ -11,12 +11,13 @@ if strcmp(plotType,'whiteBG')
     plotParams.cArena    = [ 1, 1, 1];      % arena color
     plotParams.cTarget   = [.7,.7,.7];      % fill color for targets 
     plotParams.cObstacle = [.8, 0,.4];      % fill color for obstacles 
-    plotParams.cAnchor   = [.5, 0, 0];      % marker color for anchors
-    plotParams.cTraj     = [.5, 0, 0];      % line color for trajectory
+    plotParams.cAnchor   = [ 0, 0, 0];      % marker color for anchors
+    plotParams.cTraj     = [ 0, 0, 0];      % line color for trajectory
     plotParams.cBoundary = [ 0, 0, 0];      % line color for boundary
     plotParams.ms = 12;                     % marker size
     plotParams.fs = 16;                     % font size
     plotParams.lw = 2;                      % linewidth
+    plotParams.windowSize = 4;              % window used for averaging single-trial data
 
 %elseif strcmp(plotType,'blackBG')          % uncomment to define plot
                                             %      parameters for black background
