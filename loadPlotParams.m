b1d6530a104cb708f07feff9fd301d35120ffc37
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-04-09 14:23:38 -0400

    added option for black BG

--- loadPlotParams.m
+++ loadPlotParams.m
@@ -19,8 +19,18 @@ if strcmp(plotType,'whiteBG')
     plotParams.lw = 2;                      % linewidth
     plotParams.windowSize = 4;              % window used for averaging single-trial data
 
-%elseif strcmp(plotType,'blackBG')          % uncomment to define plot
-                                            %      parameters for black background
+elseif strcmp(plotType,'blackBG')  
+    plotParams.cFig      = [ 0, 0, 0];      % figure color
+    plotParams.cArena    = [ 0, 0, 0];      % arena color
+    plotParams.cTarget   = [.5,.5,.5];      % fill color for targets 
+    plotParams.cObstacle = [.8, 0,.4];      % fill color for obstacles 
+    plotParams.cAnchor   = [ 1, 1, 1];      % marker color for anchors
+    plotParams.cTraj     = [ 1, 1, 1];      % line color for trajectory
+    plotParams.cBoundary = [ 1, 1, 1];      % line color for boundary
+    plotParams.ms = 12;                     % marker size
+    plotParams.fs = 16;                     % font size
+    plotParams.lw = 2;                      % linewidth
+    plotParams.windowSize = 4;              % window used for averaging single-trial data
 else
     error('unrecognized plot type');
 end
