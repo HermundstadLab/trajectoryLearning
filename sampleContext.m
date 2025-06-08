098fae021bda96b927231e05598597d01c7225a2
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-06-08 08:58:44 -0400

    added sampling options
    added options for MAP and proportional sampling from context belief

--- sampleContext.m
+++ sampleContext.m
@@ -1,20 +1,29 @@
-function [prior,contextEstimate] = sampleContext(contextPrior,contextIDs,cache,belief)
+function [prior,contextToSample] = sampleContext(contextPrior,contextIDs,cache,belief)
 % SAMPLECONTEXT Sample anchor points from a probability distribution.
 %
-%   [prior,contextEstimate] = SAMPLECONTEXT(contextPrior,cache,belief) uses
-%   the prior over contexts ('contextPrior') to estimate the current context,
-%   ('contextEstimate'), which is then used to select the appropriate prior
-%   belief ('prior') from the  agent's cache.
+%   [prior,contextToSample] = SAMPLECONTEXT(contextPrior,cache,belief) uses
+%   the prior over contexts ('contextPrior') to decide which context to 
+%   sample, ('contextToSample'), which is then used to select the 
+%   appropriate prior belief ('prior') from the  agent's cache.
 
 
-if strcmp(belief.cacheSamplingMethod,'MAP')
-    [~,contextIndex] = max(contextPrior);
-elseif strcmp(belief.cacheSamplingMethod,'prop')
-    c = rand();
-    contextIndex = find(histcounts(c,[0,cumsum(contextPrior)])); 
+if strcmp(belief.cacheSamplingMethod,'avg')
+    prior = zeros(belief.np);
+    for i=1:size(cache,3)
+        prior = prior+contextPrior(i).*cache(:,:,i);
+    end
+    contextToSample = sum(contextPrior.*contextIDs);
 else
-    error('unrecognized sampling method')
+    if strcmp(belief.cacheSamplingMethod,'MAP')
+        [~,contextIndex] = max(contextPrior);
+    elseif strcmp(belief.cacheSamplingMethod,'prop')
+        c = rand();
+        contextIndex = find(histcounts(c,[0,cumsum(contextPrior)])); 
+    else
+        error('unrecognized sampling method')
+    end
+
+    prior = squeeze(cache(:,:,contextIndex));
+    contextToSample = contextIDs(contextIndex); 
 end
     
-prior = squeeze(cache(:,:,contextIndex));
-contextEstimate = contextIDs(:,contextIndex); 
