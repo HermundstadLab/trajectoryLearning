6ab152d11aa97594fb92312a511d0fb16143f759
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-09-13 19:00:03 -0400

    modified visualization
    added option to plot in regular or scatter format; made default grid of nx10 subplots

--- plottingFunctions/plotSingleAgentResults_trajectories.m
+++ plottingFunctions/plotSingleAgentResults_trajectories.m
@@ -1,4 +1,8 @@
-function plotSingleAgentResults_trajectories(res,environment,agent,trial,trialIDs,plotParams)
+function plotSingleAgentResults_trajectories(res,environment,agent,trial,trialIDs,plotParams,plotType)
+
+if nargin<7
+    plotType = 'cart';
+end
 
 %---------------------- extract agent structures -------------------------%
 belief  = agent.belief;
@@ -10,17 +14,19 @@ arena   = environment.arena;
 
 %----------------------------- plot results ------------------------------%
 figure;
-set(gcf,'color','w','units','normalized','Position',[.025,.025,.95,.4]);
+set(gcf,'color','w','units','normalized','Position',[.025,.025,.95,.95]);
 nTrials = numel(trialIDs);
+
+nCols = 10;
+nRows = ceil(nTrials/nCols);
 
 for i=1:nTrials
    
     trialID = trialIDs(i);
     executedTrajectory  = res.trajectory.executed.path{trialID};
 
-    subplot(1,nTrials,i)
-    plotTrajectory(executedTrajectory,arena,planner,trial,trialID,plotParams,'executed','cart');
-    hold on; plotAnchors(executedTrajectory,arena,belief,sampler,planner,plotParams,'cart');
-    title(['reward = ',num2str(res.trajectory.rewards(trialID))]);
+    subplot(nRows,nCols,i)
+    plotTrajectory(executedTrajectory,arena,planner,trial,trialID,plotParams,'executed',plotType);
+    hold on; plotAnchors(executedTrajectory,arena,belief,sampler,planner,plotParams,plotType);
     set(gca,'fontsize',plotParams.fs);
 end
