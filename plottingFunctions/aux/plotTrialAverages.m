cf9ff767bd572b82ba0cc3056e1806a6b127807a
Ann Hermundstad <hermundstada@janelia.hhmi.org>
2025-09-14 07:19:59 -0400

    cleaned up commented code

--- plottingFunctions/aux/plotTrialAverages.m
+++ plottingFunctions/aux/plotTrialAverages.m
@@ -13,36 +13,17 @@ if nargin<7
     end
 end
 
-[~,nAgents] = size(ydat);
-
 % compute averages
-%d = movmean(ydat,[plotParams.windowSize,0],1);
 if smooth
-    d = movmean(ydat,7,1);
+    d = movmean(ydat,plotParams.windowSize,1);
 else
     d = ydat;
 end
 davg = mean(d,2);
-dste = std(d,[],2)./sqrt(nAgents);
-
-% create vectors for patch objects
-xvec = createPatchVec(xdat);
-yvec = createPatchVec(davg',dste');
-
-% plot results
-%h = fill(xvec,yvec,color,'edgecolor','none');
-%set(h,'facealpha',.5);
 
 hold on;plot(xdat,davg,linestyle,'color',color,'linewidth',plotParams.lw);
 xlabel('trials');
 ylabel(yaxisLabel)
 set(gca,'fontsize',16)
 
-end
-
-function patchVec = createPatchVec(vec,offset)
-if nargin<2
-    offset = zeros(size(vec));
-end
-patchVec = [vec-offset,fliplr(vec+offset),vec(1)-offset(1)];
 end
